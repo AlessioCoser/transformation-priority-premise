9c54db302b48815b388646fda97624c35eaaddcb
Alessio Coser <alessio.coser@xpeppers.com>
2018-02-09 12:01:16 +0100

    removes duplication

--- src/main/java/com/alessiocoser/WordWrap.java
+++ src/main/java/com/alessiocoser/WordWrap.java
@@ -3,24 +3,25 @@ package com.alessiocoser;
 public class WordWrap {
 
     public String wrap(String text, Integer length) throws InvalidArgument {
-        if (length < 1) {
+        if (length < 1)
             throw new InvalidArgument();
-        }
 
-        if (text == null) {
+        if (text == null)
             return "";
-        }
 
-        if (text.length() <= length) {
+        if (text.length() <= length)
             return text;
-        }
 
         int space = text.indexOf(" ");
         if (space >= 0) {
-            return text.substring(0, space) + "\n" + wrap(text.substring(space + 1), length);
+            return breakBetween(text, space, space + 1, length);
         }
 
-        return text.substring(0, length) + "\n" + wrap(text.substring(length), length);
+        return breakBetween(text, length, length, length);
+    }
+
+    private String breakBetween(String text, Integer start, Integer end, Integer length) throws InvalidArgument {
+        return text.substring(0, start) + "\n" + wrap(text.substring(end), length);
     }
 
     public class InvalidArgument extends Exception {
