e6d106de48cf478d2c3980ac564fb5553f9516b2
Alessio Coser <alessio.coser@xpeppers.com>
2018-02-09 10:38:45 +0100

    removes automatic comments above test class

--- src/test/java/com/alessiocoser/WordWrapKataTest.java
+++ src/test/java/com/alessiocoser/WordWrapKataTest.java
@@ -5,9 +5,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by alessiocoser on 09/02/2018.
- */
 public class WordWrapKataTest {
     @Test
     public void xxx() {
