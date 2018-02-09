09389ee007ffa4980c7f541cdadc3a86f29c6341
Alessio Coser <alessio.coser@xpeppers.com>
2018-02-09 10:52:23 +0100

    extracts class initialization

--- src/test/java/com/alessiocoser/WordWrapKataTest.java
+++ src/test/java/com/alessiocoser/WordWrapKataTest.java
@@ -1,22 +1,27 @@
 package com.alessiocoser;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class WordWrapKataTest {
+
+    private WordWrap wordWrap;
+
+    @Before
+    public void setUp() throws Exception {
+        wordWrap = new WordWrap();
+    }
+
     @Test
     public void WrapNullReturnsEmptyString() throws Exception {
-        WordWrap wordWrap = new WordWrap();
-
         assertThat(wordWrap.wrap(null, 10), is(""));
     }
 
     @Test
     public void WrapEmptyStringReturnsEmptyString() throws Exception {
-        WordWrap wordWrap = new WordWrap();
-
         assertThat(wordWrap.wrap("", 10), is(""));
     }
 }
