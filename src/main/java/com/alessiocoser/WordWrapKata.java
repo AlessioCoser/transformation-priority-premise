ac8c2ef9027bc7df9c9fef8cdc4a29af09ed0618
Alessio Coser <alessio.coser@xpeppers.com>
2018-02-09 10:35:23 +0100

    configure gradle to run test

---
+++ src/main/java/com/alessiocoser/WordWrapKata.java
@@ -0,0 +1,8 @@
+package com.alessiocoser;
+
+public class WordWrapKata {
+
+    public static void main(String[] args) {
+        System.out.println("Hello WordWrapKata!");
+    }
+}
---
+++ src/test/java/com/alessiocoser/WordWrapKataTest.java
@@ -0,0 +1,27 @@
+package com.alessiocoser;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by alessiocoser on 09/02/2018.
+ */
+public class WordWrapKataTest {
+    @Test
+    public void xxx() {
+        assertEquals(1, 12);
+    }
+
+    @Test
+    public void yyy() {
+        assertEquals(1, 1);
+    }
+
+    @Ignore
+    @Test
+    public void zzz() {
+        assertEquals(1, 1);
+    }
+}
